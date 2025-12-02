3831fb9b6dba582fb018441d8356a9beaa08e064
FRANCESCO SPALLUZZI <aiutocorsi@hotmail.it>
2025-12-02 15:47:51 +0100

    Aggiornato progetto PyServiceDistanceList

--- PyServiceDistanceList/main.py
+++ PyServiceDistanceList/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from collections import Counter
 
 app = FastAPI()
 
@@ -9,7 +10,12 @@ class ListsInput(BaseModel):
 
 @app.post("/calcola")
 def calcola(data: ListsInput):
+    # PARTE 1 Calcolo distanze minime totali tra due liste
     L = sorted(data.left)
     R = sorted(data.right)
     totale = sum(abs(a - b) for a, b in zip(L, R))
-    return {"result": totale}
+
+    # PARTE 2 - Somme pesate per coincidenza valori tra le liste
+    freq_right = Counter(R)
+    total_similarity = sum(x * freq_right.get(x, 0) for x in L)
+    return {"result": totale, "similarity": total_similarity}
